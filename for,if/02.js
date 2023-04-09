f2e18cb551f2bcdb5f3d7f8625d7e4adb2e53148
junseok1925 <rkdwnstjr1925@naver.com>
2023-04-09 17:19:48 +0900

    자바스크립트 알고리즘 문제 추가

--- for,if/02.js
+++ for,if/02.js
@@ -87,4 +87,82 @@ function solution(n) {
   }
 
   
+  https://school.programmers.co.kr/learn/courses/30/lessons/12934
+
+//   정수 제곱근 판별
+//   문제 설명
+//   임의의 양의 정수 n에 대해, n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하려 합니다.
+//   n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
   
+//   제한 사항
+//   n은 1이상, 50000000000000 이하인 양의 정수입니다.
+
+function solution(n) {
+    var answer = 0;
+    let sqrt = Math.sqrt(n);
+    if (sqrt % 1 !== 0) { // 제곱근이 아닐 경우 소숫점이 나온다
+        answer = -1;      // -1를 리턴
+    } else {
+        answer = Math.pow(sqrt+1,2); // 제곱근일 경우 (sqrt+1)에 2를 제곱한 값을 리턴한다
+    }
+    return answer
+}
+
+// 사용된 메서드 정리
+
+// Math.sqrt(x) : 숫자 x의 제곱근을 반환한다.
+// Math.pow(base, exponent) : base에 exponent를 제곱한 값을 반환한다.
+
+https://school.programmers.co.kr/learn/courses/30/lessons/12935
+
+// 제일 작은 수 제거하기
+// 문제 설명
+// 정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수, solution을 완성해주세요. 단, 리턴하려는 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴하세요. 예를들어 arr이 [4,3,2,1]인 경우는 [4,3,2]를 리턴 하고, [10]면 [-1]을 리턴 합니다.
+
+// 제한 조건
+// arr은 길이 1 이상인 배열입니다.
+// 인덱스 i, j에 대해 i ≠ j이면 arr[i] ≠ arr[j] 입니다.
+
+function solution(arr) {
+    
+    if (arr.length == 1) {
+        return [-1];
+    }
+    arr.sort((a, b) => b - a);
+    arr.pop();
+    return arr;
+    
+}
+
+//실행결과는 통과했으나 유효성검사에서 탈락
+
+https://school.programmers.co.kr/learn/courses/30/lessons/12943
+
+// 콜라츠 추측
+// 문제 설명
+// 1937년 Collatz란 사람에 의해 제기된 이 추측은, 주어진 수가 1이 될 때까지 다음 작업을 반복하면, 모든 수를 1로 만들 수 있다는 추측입니다. 작업은 다음과 같습니다.
+
+// 1-1. 입력된 수가 짝수라면 2로 나눕니다. 
+// 1-2. 입력된 수가 홀수라면 3을 곱하고 1을 더합니다. 
+// 2. 결과로 나온 수에 같은 작업을 1이 될 때까지 반복합니다. 
+// 예를 들어, 주어진 수가 6이라면 6 → 3 → 10 → 5 → 16 → 8 → 4 → 2 → 1 이 되어 총 8번 만에 1이 됩니다. 위 작업을 몇 번이나 반복해야 하는지 반환하는 함수, solution을 완성해 주세요. 단, 주어진 수가 1인 경우에는 0을, 작업을 500번 반복할 때까지 1이 되지 않는다면 –1을 반환해 주세요.
+
+function solution(num) {
+    var answer = 0;
+    for(let i = 0; i <= 500; i++){
+       if (num % 2 == 0){
+            num /= 2;
+        } else {
+            num = (num * 3) + 1;
+        }
+        answer += 1;
+        return answer;
+    }
+    
+    if (answer > 500) {
+        return -1;
+    } else {
+        return answer;
+    }
+   
+}
