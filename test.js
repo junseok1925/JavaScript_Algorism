3f7217865b714a7ddbbfe642806408934eccf4e7
junseok1925 <rkdwnstjr1925@naver.com>
2023-04-15 00:24:34 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-
-(function test(){
-const testArray = [1,2,3,4,5];
-const newArray = testArray.filter(function(element, index, array){
-    console.log(element);
-    console.log(index);
-    console.log(array);
-    });
-})(); 
+async function getMyName() {
+    return Promise.resolve('강승현');
+  }
+  
+  const result = getMyName();
+  
+  result.then((myName) => {
+    console.log(myName);
+  });
