6db8994c41532c79346f3b4b5cf0517b8f737336
junseok1925 <rkdwnstjr1925@naver.com>
2023-04-15 20:18:49 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,29 @@
-async function getMyName() {
-    return Promise.resolve('강승현');
+function solution(N, arr1){
+  let x = [1, -1, 0, 0, 1, 1, -1, -1];
+  let y = [0, 0, 1, -1, 1, -1, 1, -1];
+  let answer = [];
+  for(let i = 0; i < N; i++){
+    let line = [];
+    for(let j = 0; j < N; j++){
+      if(arr1[i][j] === '*') line.push('*');
+      else{
+        let count = 0;
+        for(let k = 0; k < 8; k++){
+          let nx = i + x[k];
+          let ny = j + y[k];
+          if(nx >= 0 && nx < N && ny >= 0 && ny < N && arr1[nx][ny] === '*') count++;
+        }
+        line.push(count);
+      }
+    }
+    answer.push(line);
   }
-  
-  const result = getMyName();
-  
-  result.then((myName) => {
-    console.log(myName);
-  });
+  return answer;
+}
+let N = 5;
+let arr1 = [['1', '.', '.', '.', '.'],
+['.', '.', '3', '.', '.'],
+['.', '.', '.', '.', '.'],
+['.', '4', '.', '.', '.'],
+['.', '.', '.', '9', '.']];
+console.log(solution(N, arr1));
